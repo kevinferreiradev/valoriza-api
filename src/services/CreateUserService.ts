cdba719e86dfe3e82e90785766c9889c3c9678c1
kevinferreiradev <ferreirakevin325@gmail.com>
2022-01-29 05:54:22 -0300

    created controllers

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,3 +1,5 @@
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
 interface IUserRequest {
     name: string;
     email: string;
@@ -5,8 +7,30 @@ interface IUserRequest {
 }
 
 class CreateUserService {
-   async ({name, email, admin} : IUserRequest) {
+   async execute({name, email, admin} : IUserRequest) {
+       const usersRepository = new UsersRepositories();
        
+       if(!email) {
+           throw new Error("Email incorreto");
+       }
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        });
+
+        if(userAlreadyExists) {
+            throw new Error ("User already exists")
+        }
+       
+      const user = usersRepository.create({
+          name,
+          email,
+          admin
+      })
+
+      await usersRepository.save(user);
+
+      return user;
    }
 }
 
